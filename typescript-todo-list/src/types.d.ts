a9da3cc3acfcef197104216776e0b57ba7c1b440
Your Name <you@example.com>
2026-01-21 16:12:10 +0530

    Introduced Discriminated Unions for better API handling

--- typescript-todo-list/src/types.d.ts
+++ typescript-todo-list/src/types.d.ts
@@ -1,1 +1,24 @@
-types MS = `${number}ms`;
+
+
+// Discriminated unions
+interface LoadingState {
+    status: "loading";
+}
+
+interface SuccessState {
+    status: "success";
+    data: User[];
+}
+
+interface ErrorState {
+    status: "error";
+    error: string;
+}
+
+// The discriminated union type
+type ApiResponse = LoadingState | SuccessState | ErrorState;
+
+interface User {
+    name: string;
+    age: number;
+}
